50ad4278465ffb9269e4f6772bb4f657c7058428
thkim0515 <thkim0515@naver.com>
2024-06-30 19:21:57 +0900

    fixed:properties 수정

--- src/main/java/com/example/demo/PropertiesAPI/PropertiesProxy.java
+++ src/main/java/com/example/demo/PropertiesAPI/PropertiesProxy.java
@@ -1,57 +1,73 @@
 package com.example.demo.PropertiesAPI;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-
-
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class PropertiesProxy {
-    private final String API_KEY = "fnHXpQdXcUKuxNNB4JhjrTvdJrhjQ4gRUNzOLG9gwV4FeAFxzoE4A0TkbnDp%2FjptXWsbP4tx7gdqT%2B6nVcKWKQ%3D%3D";
-    private final String BASE_URL = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?_wadl&type=xml";
+    @Value("${properties.API_KEY}")
+    private String API_KEY;
+
+    @Value("${properties.BASE_URL}")
+    private String BASE_URL;
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/api/data")
     public ResponseEntity<Map<String, Object>> getData(@RequestBody RequestParams params) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = String.format("%s?serviceKey=%s&pageNo=%s&numOfRows=%s&LAWD_CD=%s&DEAL_YMD=%s",
-                BASE_URL, API_KEY, params.getPageNo(), params.getNumOfRows(), params.getLawdCd(), params.getDealYmd());
-
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-
-        // XML 응답을 JSON으로 변환
-        XmlMapper xmlMapper = new XmlMapper();
-        Map<String, Object> jsonResponse = null;
+        Map<String, Object> jsonResponse = new HashMap<>();
         try {
-            jsonResponse = xmlMapper.readValue(response.getBody(), Map.class);
-        } catch (JsonProcessingException e) {
+            StringBuilder urlBuilder = new StringBuilder(BASE_URL);
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + API_KEY);
+            urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(params.getPageNo(), "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode(params.getNumOfRows(), "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("LAWD_CD", "UTF-8") + "=" + URLEncoder.encode(params.getLawdCd(), "UTF-8"));
+            urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD", "UTF-8") + "=" + URLEncoder.encode(params.getDealYmd(), "UTF-8"));
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            // XML 응답을 JSON으로 변환
+//            jsonResponse.put("response", sb.toString());
+
+            // XML 응답을 JSON으로 변환
+            XmlMapper xmlMapper = new XmlMapper();
+            JsonNode jsonNode = xmlMapper.readTree(sb.toString().getBytes());
+            jsonResponse.put("response", jsonNode);
+
+
+        } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.status(500).body(Map.of("error", "Failed to parse XML"));
+            return ResponseEntity.status(500).body(Map.of("error", "Failed to fetch data"));
         }
 
         return ResponseEntity.ok(jsonResponse);
-//        // 응답 헤더에 UTF-8 인코딩 설정
-//        HttpHeaders responseHeaders = new HttpHeaders();
-//        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
-//        responseHeaders.set("Content-Type", "application/json; charset=UTF-8");
-//
-//        return ResponseEntity.ok()
-//                .headers(responseHeaders)
-//                .body(response.getBody());
     }
 }
 
