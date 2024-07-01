382fdd70c96d260c6906525b19f28259c44b5f09
thkim0515 <thkim0515@naver.com>
2024-07-01 19:06:55 +0900

    feat:AWS S3 Upload 로직

--- src/main/java/com/example/demo/FileAttachment/controller/PostController.java
+++ src/main/java/com/example/demo/FileAttachment/controller/PostController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.FileAttachment.controller;
 
 import com.example.demo.FileAttachment.entity.FileAttachment;
 import com.example.demo.FileAttachment.repository.FileAttachmentRepository;
